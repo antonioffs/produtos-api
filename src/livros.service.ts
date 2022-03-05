803b2401cd5e95662faef752c24cb066dd2cd26d
Antonio Silva <antonio.ffsmail@gmail.com>
2022-03-05 17:13:59 -0300

    Commented the mock list of books

--- src/livros.service.ts
+++ src/livros.service.ts
@@ -5,9 +5,9 @@ import { Livro } from "./livro.model";
 export class LivrosService{
 
     livros: Livro[] = [
-        new Livro('LIV01', 'TDD e BDD na prática', 29.90),
-        new Livro('LIV02', 'Iniciando com Flutter', 49.90),
-        new Livro('LIV03', 'Inteligência artificial como serviço', 39.90),
+        // new Livro('LIV01', 'TDD e BDD na prática', 29.90),
+        // new Livro('LIV02', 'Iniciando com Flutter', 49.90),
+        // new Livro('LIV03', 'Inteligência artificial como serviço', 39.90),
     ]
 
     obterTodos(): Livro[]{
